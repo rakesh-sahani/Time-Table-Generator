7dbe531251c90f4984d2423021b50eaa2b1cfd50
Rakesh Sahani <rakeshsahani@live.in>
2018-04-04 12:45:23 +0530

    Add files via upload

---
+++ TimeTable.sql
@@ -0,0 +1,92 @@
+/*
+SQLyog Community v12.06 (32 bit)
+MySQL - 6.0.0-alpha-community-nt-debug : Database - timetable
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`timetable` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `timetable`;
+
+/*Table structure for table `admin` */
+
+DROP TABLE IF EXISTS `admin`;
+
+CREATE TABLE `admin` (
+  `ID` int(11) NOT NULL,
+  `UserName` varchar(10) DEFAULT NULL,
+  `Password` varchar(10) DEFAULT NULL,
+  `AcessLevel` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `admin` */
+
+insert  into `admin`(`ID`,`UserName`,`Password`,`AcessLevel`) values (1,'Admin','Admin','Admin');
+
+/*Table structure for table `course` */
+
+DROP TABLE IF EXISTS `course`;
+
+CREATE TABLE `course` (
+  `cid` int(11) NOT NULL AUTO_INCREMENT,
+  `course` varchar(10) DEFAULT NULL,
+  `year` varchar(10) DEFAULT NULL,
+  `sem` varchar(10) DEFAULT NULL,
+  `scode` varchar(20) DEFAULT NULL,
+  `sname` varchar(40) DEFAULT NULL,
+  `lname` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`cid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `course` */
+
+/*Table structure for table `faculty` */
+
+DROP TABLE IF EXISTS `faculty`;
+
+CREATE TABLE `faculty` (
+  `fid` int(11) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(40) DEFAULT NULL,
+  `mob` varchar(20) DEFAULT NULL,
+  `dept` varchar(40) DEFAULT NULL,
+  `password` varchar(40) DEFAULT NULL,
+  `acesslevel` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`fid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `faculty` */
+
+insert  into `faculty`(`fid`,`fullname`,`mob`,`dept`,`password`,`acesslevel`) values (1,'Uzma Ali','9999999999','IT ','UzmaAli','Faculty');
+
+/*Table structure for table `timetable` */
+
+DROP TABLE IF EXISTS `timetable`;
+
+CREATE TABLE `timetable` (
+  `tid` int(11) NOT NULL AUTO_INCREMENT,
+  `Course` varchar(10) DEFAULT NULL,
+  `Year` varchar(10) DEFAULT NULL,
+  `Semester` varchar(10) DEFAULT NULL,
+  `Day` varchar(10) DEFAULT NULL,
+  `StartTime` varchar(20) DEFAULT NULL,
+  `EndTime` varchar(20) DEFAULT NULL,
+  `Subject` varchar(20) DEFAULT NULL,
+  `Lecturer` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`tid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `timetable` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
